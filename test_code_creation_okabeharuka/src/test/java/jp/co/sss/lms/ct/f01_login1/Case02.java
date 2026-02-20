9a42b381b730446697acef1c82385a9321a8c94d
h-okabe <h-okabe@PLPC34>
2026-02-20 09:57:56 +0900

    fix:Case02.java  修正
    44-53行目：ログイン画面への遷移テストコード追加
    WebDriverで取得したタイトルタグの値が同一かHTMLのタグと一致しているか
    確認するテストコードを作成。
    【期待値】
    タイトルタグ："ログイン | LMS"
    
    60-91行目：存在しないアカウント情報でのログインテストコード追加
    WebDriverで取得したname属性にアカウント情報を送り、ログインボタンをクリックしてログインが失敗するか確認するテストコードを作成。
    【期待値】
    1.「* ログインに失敗しました。」というエラーメッセージが表示される
    2.ログイン後の画面に遷移しない

--- test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +41,54 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		
+		WebDriverWait wait = new WebDriverWait(webDriver,Duration.ofSeconds(10)); 
+		
+		webDriver.get("http://localhost:8080/lms");
+
+		wait.until(ExpectedConditions.titleIs("ログイン | LMS"));
+		
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		
+		WebDriverWait wait = new WebDriverWait(webDriver,Duration.ofSeconds(10)); 
+		
+		webDriver.get("http://localhost:8080/lms");
+
+		wait.until(ExpectedConditions.titleIs("ログイン | LMS"));
+		
+		//DBに存在しないIDとパスワードを入力してテスト
+		webDriver.findElement(By.name("loginId")).sendKeys("1111111111");
+		webDriver.findElement(By.name("password")).sendKeys("1111111111");
+		webDriver.findElement(By.className("btn-primary")).click();
+		
+		getEvidence(new Object(){}, "NotUser");
+		
+		//DBに存在するIDと存在しないパスワードを入力してテスト
+		webDriver.findElement(By.name("loginId")).clear();
+		webDriver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.name("password")).clear();
+		webDriver.findElement(By.name("password")).sendKeys("1111111111");
+		webDriver.findElement(By.className("btn-primary")).click();
+		
+		getEvidence(new Object(){}, "NotFoundPassword");
+		
+		//DBに存在しないIDと存在するパスワードを入力してテスト
+		webDriver.findElement(By.name("loginId")).clear();
+		webDriver.findElement(By.name("loginId")).sendKeys("1111111111");
+		webDriver.findElement(By.name("password")).clear();
+		webDriver.findElement(By.name("password")).sendKeys("StudentAA01");
+		webDriver.findElement(By.className("btn-primary")).click();
+		
+		getEvidence(new Object(){}, "NotFoundId");
+		
 	}
 
 }
