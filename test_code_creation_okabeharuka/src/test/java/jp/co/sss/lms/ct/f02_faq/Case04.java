6d4d8a19f9474d936243b70b601e51fb3c7bc324
h-okabe <h-okabe@PLPC34>
2026-02-20 17:10:07 +0900

    fix:Case04.java  修正
    43-49行目：ログイン画面への遷移テストコード追加
    WebDriverで取得したタイトルタグの値が同一かHTMLのタグと一致しているか
    確認するテストコードを作成。
    【期待値】
    タイトルタグ："ログイン | LMS"
    
    57-64行目：初回ログイン済みの受講生ユーザーでログインテストコード追加
    WebDriverで取得したname属性にアカウント情報を送り、ログインボタンをクリックしてログインが成功するか確認するテストコードを作成。
    【期待値】
    タイトルタグ："コース詳細 | LMS"
    
    71-83行目：「ヘルプ」リンクからヘルプ画面に遷移テストコード追加
    WebDriverで取得したaタグをクリックし、コース詳細画面上部のメニューから、
    ヘルプ画面へ遷移するテストコードを作成。
    【期待値】
    タイトルタグ："ヘルプ | LMS"
    
    90-100行目：よくある質問」リンクからよくある質問画面に遷移テストコード追加
    WebDriverで取得したaタグをクリックし、コース詳細画面上部のメニューから、
    よくある質問画面へ遷移するテストコードを作成。
    【期待値】
    タイトルタグ："よくある質問 | LMS"

--- test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +40,64 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		
+		goTo("http://localhost:8080/lms");		
+		
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		
+		getEvidence(new Object(){});
+		
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		
+		webDriver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.name("password")).sendKeys("StudentAA001");		
+		webDriver.findElement(By.className("btn-primary")).click();		
+		
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());	
+		
+		getEvidence(new Object(){});			
+		
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		
+		webDriver.findElement(By.linkText("機能")).click();	
+		
+		visibilityTimeout(By.linkText("ヘルプ"), 10);
+		
+		webDriver.findElement(By.linkText("ヘルプ")).click();
+		
+		pageLoadTimeout(10);
+		
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());	
+		
+		getEvidence(new Object(){});
+		
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		
+		webDriver.findElement(By.linkText("よくある質問")).click();	
+		List<String> handleList = new ArrayList<>(webDriver.getWindowHandles());
+		webDriver.switchTo().window(handleList.get(1));		
+		
+		visibilityTimeout(By.cssSelector("h2"), 10);
+		
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());	
+		
+		getEvidence(new Object(){});
+		
 	}
 
 }
