5a9cf13e999020c5bccc5c001786ab2e1ebd64c3
h-okabe <h-okabe@PLPC34>
2026-02-20 11:26:11 +0900

    fix:Case01.java  修正
    ログイン画面表示の待ち処理の記述をWebDriverUtilsクラスの
    visibilityTimeout()メソッドへ修正。

--- test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -3,8 +3,6 @@ package jp.co.sss.lms.ct.f01_login1;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Duration;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -40,11 +37,10 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		WebDriverWait wait = new WebDriverWait(webDriver,Duration.ofSeconds(10)); 
 		
 		webDriver.get("http://localhost:8080/lms");
-
-		wait.until(ExpectedConditions.titleIs("ログイン | LMS"));
+		
+		visibilityTimeout(By.tagName("h2"), 10);
 		
 		assertEquals("ログイン | LMS", webDriver.getTitle());
 		
