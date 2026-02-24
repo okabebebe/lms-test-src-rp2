b1af9bebfc3c651ff5c340cfa3eb5aa9a47f0a3f
h-okabe <h-okabe@PLPC34>
2026-02-24 16:07:57 +0900

    fix:Case06.java  修正
    44-50行目：ログイン画面への遷移テストコード追加
    WebDriverで取得したタイトルタグの値が同一かHTMLのタグと一致しているか
    確認するテストコードを作成。
    【期待値】
    タイトルタグ："ログイン | LMS"
    
    57-67行目：初回ログイン済みの受講生ユーザーでログインテストコード追加
    WebDriverで取得したname属性にアカウント情報を送り、ログインボタンをクリックしてログインが成功するか確認するテストコードを作成。
    【期待値】
    タイトルタグ："コース詳細 | LMS"
    
    74-91行目：「ヘルプ」リンクからヘルプ画面に遷移テストコード追加
    WebDriverで取得したaタグをクリックし、コース詳細画面上部のメニューから、
    ヘルプ画面へ遷移するテストコードを作成。
    【期待値】
    タイトルタグ："ヘルプ | LMS"
    
    98-115行目：「よくある質問」リンクからよくある質問画面に遷移テストコード追加
    WebDriverで取得したaタグをクリックし、コース詳細画面上部のメニューから、
    よくある質問画面へ遷移するテストコードを作成。
    【期待値】
    タイトルタグ："よくある質問 | LMS"
    
    122-137行目：カテゴリ検索で該当カテゴリの検索結果だけ表示されるテストコード追加
    WebDriverで【研修関係】リンクをクリック、カテゴリーの質問がすべて表示されるテストコードを作成。
    【期待値】
    テキスト："Q.キャンセル料・途中退校について"、"Q.研修の申し込みはどのようにすれば良いですか？"
    
    144-161行目：検索結果の質問をクリックしその回答を表示されるテストコード追加。
    WebDriverで検索結果の質問をクリックし、質問文の下に回答文が表示されるテストコード作成。
    【期待値】
    テキスト：A. 受講者の退職や解雇等、やむを得ない事情による途中終了に関してなど、事情をお伺いした上で、協議という形を取らせて頂きます。
    弊社営業担当までご相談下さい。"

--- test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_okabeharuka/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,42 +41,124 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		
+		goTo("http://localhost:8080/lms");		
+		
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		
+		getEvidence(new Object(){});
+		
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+	
+		//初回ログイン済みの受講生ユーザーを入力してテスト
+		webDriver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.name("password")).sendKeys("StudentAA001");		
+		webDriver.findElement(By.className("btn-primary")).click();		
+		
+		//タイトルタグの記述が一致しているか確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());	
+		
+		getEvidence(new Object(){});	
+		
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		
+		//ヘッダー内にある「機能」リンクをクリック
+		webDriver.findElement(By.linkText("機能")).click();	
+		
+		//「ヘルプ」リンクが出てくるまで待ち処理
+		visibilityTimeout(By.linkText("ヘルプ"), 10);
+		
+		//「ヘルプ」リンクをクリック
+		webDriver.findElement(By.linkText("ヘルプ")).click();	
+		
+		//ページロードのタイムアウト処理
+		pageLoadTimeout(10);
+		
+		//タイトルタグの記述が一致しているか確認
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());	
+		
+		getEvidence(new Object(){});
+		
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		
+		//「よくある質問」リンクをクリック
+		webDriver.findElement(By.linkText("よくある質問")).click();	
+		
+		//現在の各タブの番号をすべて取得
+		List<String> handleList = new ArrayList<>(webDriver.getWindowHandles());
+		
+		//もとから開いていた1番目のタブから後から開いた2番目のタブへ切り替え
+		webDriver.switchTo().window(handleList.get(1));		
+		
+		//2番目のタブのh2が表示されるまでの待ち処理
+		visibilityTimeout(By.cssSelector("h2"), 10);
+		
+		//タイトルタグの記述が一致しているか確認
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());	
+		
+		getEvidence(new Object(){});
+		
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		
+		//「【研修関係】」リンクをクリック
+		webDriver.findElement(By.linkText("【研修関係】")).click();
+		
+		//ページロードのタイムアウト処理
+		pageLoadTimeout(10);
+		
+		//検索結果をすべて取得
+		List<WebElement> results = webDriver.findElements(By.className("mb10"));
+		
+		//検索結果と期待する結果にて表示される文言が同一かチェック
+		assertEquals("Q.キャンセル料・途中退校について", results.get(0).getText());
+		assertEquals("Q.研修の申し込みはどのようにすれば良いですか？", results.get(1).getText());
+		
+		getEvidence(new Object(){});
+		
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 検索結果の質問をクリックしその回答を表示")
 	void test06() {
-		// TODO ここに追加
+		
+		//指定位置までスクロール
+		scrollTo("500");
+		
+		//検索結果をすべて取得し、各質問文をクリック
+		List<WebElement> questions = webDriver.findElements(By.className("mb10"));
+		questions.get(0).click();
+		
+		String cancel = "A. 受講者の退職や解雇等、やむを得ない事情による途中終了に関してなど、事情をお伺いした上で、協議という形を取らせて頂きます。 弊社営業担当までご相談下さい。";
+		
+		//各質問文の下の回答をすべて取得
+		List<WebElement> questionsTexts = webDriver.findElements(By.id("answer-h[${status.index}]"));
+		
+		//検索結果と期待する結果にて表示される文言が同一かチェック
+		assertEquals(cancel, questionsTexts.get(0).getText());		
+		
+		getEvidence(new Object(){});
+		
 	}
 
 }
